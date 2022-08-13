182b6cd77254c930fd840edb740be9300bed6eda
Sayan Chowdhury <sayan.chowdhury.aiml25@heritageit.edu.in>
2022-08-13 12:46:32 +0530

    Add tests

--- test/StableCointToken.test.ts
+++ test/StableCointToken.test.ts
@@ -8,74 +8,25 @@ import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { StableCoinToken, StableCoinToken__factory } from "../typechain-types";
 
 describe("StableCoinToken", function () {
-  let stableCoinToken: StableCoinToken;
-  let owner: SignerWithAddress;
-  let addr1: SignerWithAddress;
-  let addr2: SignerWithAddress;
-  let addrs: SignerWithAddress[];
+  let contract: StableCoinToken;
 
-  beforeEach(async function () {
-    [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
-    const stableCoinTokenFactory = (await ethers.getContractFactory(
-      "Stable",
-      owner
-    )) as StableCoinToken__factory;
-
-   
-    stableCoinToken = await stableCoinTokenFactory.deploy();
-    await 
+  beforeEach(async () => {
+    const StableCoinToken = await ethers.getContractFactory("StableCoinToken");
+    contract = await StableCoinToken.deploy("Stable Token", "STC");
   });
-  describe("Deployment", function () {
-    it("Should assign the total supply of tokens to the owner", async function () {
-      const ownerBalance = await linkToken.balanceOf(owner.address);
-      expect(await linkToken.totalSupply()).to.equal(ownerBalance);
+  describe("Minting", () => {
+    it("Should Mint", async () => {
+      await contract.deployed();
     });
   });
-  describe("Transactions", function () {
-    it("Should transfer tokens between accounts", async function () {
-      // Transfer 50 tokens from owner to addr1
-      await linkToken.transfer(addr1.address, 50);
-      const addr1Balance = await linkToken.balanceOf(addr1.address);
-      expect(addr1Balance).to.equal(50);
-
-      // Transfer 50 tokens from addr1 to addr2
-      // We use .connect(signer) to send a transaction from another account
-      await linkToken.connect(addr1).transfer(addr2.address, 50);
-      const addr2Balance = await linkToken.balanceOf(addr2.address);
-      expect(addr2Balance).to.equal(50);
+  describe("Minting", () => {
+    it("Should Mint", async () => {
+      await contract.deployed();
     });
-    it("Should fail if sender doesn’t have enough tokens", async function () {
-      const initialOwnerBalance = await linkToken.balanceOf(owner.address);
-
-      // Try to send 1 token from addr1 (0 tokens) to owner (1000 tokens).
-      // `require` will evaluate false and revert the transaction.
-      await expect(
-        linkToken.connect(addr1).transfer(owner.address, 1)
-      ).to.be.revertedWith("ERC20: transfer amount exceeds balance");
-
-      // Owner balance shouldn't have changed.
-      expect(await linkToken.balanceOf(owner.address)).to.equal(
-        initialOwnerBalance
-      );
-    });
-    it("Should update balances after transfers", async function () {
-      const initialOwnerBalance = await linkToken.balanceOf(owner.address);
-
-      // Transfer 100 tokens from owner to addr1.
-      await linkToken.transfer(addr1.address, 100);
-
-      // Transfer another 50 tokens from owner to addr2.
-      await linkToken.transfer(addr2.address, 50);
-
-      // Check balances.
-      const finalOwnerBalance = await linkToken.balanceOf(owner.address);
-      expect(finalOwnerBalance).to.equal(initialOwnerBalance.sub(150));
-
-      const addr1Balance = await linkToken.balanceOf(addr1.address);
-      expect(addr1Balance).to.equal(100);
-
-      const addr2Balance = await linkToken.balanceOf(addr2.address);
-      expect(addr2Balance).to.equal(50);
+  });
+  describe("Minting", () => {
+    it("Should Mint", async () => {
+      await contract.deployed();
     });
   });
 });
